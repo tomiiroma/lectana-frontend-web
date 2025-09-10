bf2d14095165aa14e9fe4d0fe5b2a7b3f37b8a67
agusaguerom <agusaguerom28@gmail.com>
2025-09-10 20:55:59 -0300

    sidebar admin

--- src/components/AdminSidebar/AdminSidebar.jsx
+++ src/components/AdminSidebar/AdminSidebar.jsx
@@ -1,14 +1,11 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../auth/AuthContext";
-import "./AdminSidebar.css"
+import "./AdminSidebar.css";
+import { RiLayoutMasonryLine } from "react-icons/ri";
+import { FaBook, FaUser,FaBuilding,FaClipboardList,FaRegUser  } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 
-const linkStyle = ({ isActive }) => ({
-  fontSize: "1em",
-  fontFamily:"Inter, Inter Fallback",
-  padding: "0.5rem 0.75rem",
-  transition: "all 0.1s ease-in-out",
-  textDecoration: "none",
-});
+
 
 export default function AdminSidebar() {
   const { logout } = useAuth();
@@ -20,35 +17,100 @@ export default function AdminSidebar() {
   }
 
   return (
-    <aside style={{ width: 240, padding: 12, borderRight: "1px solid #e5e7eb", background: "#f8fafc", height: "100vh", position: "sticky", top: 0 }}>
-      <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"5px"}}>
-        <img src="/lectana.svg" alt="Logo" className="header-logo"/>
-      <h1 style={{  fontWeight: 700 ,fontSize:"1.5em", fontFamily:"Inter, Inter Fallback", color: "#0056D2" }}>Lectana Admin</h1>
+    <aside
+      style={{
+          width: 240,
+    padding: 12,
+    borderRight: "1px solid #e5e7eb",
+    background: "#f8fafc",
+    height: "100vh",
+    position: "sticky",
+    top: 0,
+    display: "flex",
+    flexDirection: "column",
+      }}
+    >
+      <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "5px",
+        }}
+      >
+        <img src="/lectana.svg" alt="Logo" className="header-logo" />
+        <h1
+          style={{
+            fontWeight: 700,
+            fontSize: "1.5em",
+            fontFamily: "Inter, Inter Fallback",
+            color: "#0056D2",
+          }}
+        >
+          Lectana Admin
+        </h1>
       </div>
-      <hr style={{width:"100%", padding:0}}/>
-      <nav style={{ display: "grid", gap: 6, marginTop:"20px"}}>
-        <NavLink to="/admin" style={linkStyle} >Dashboard</NavLink>
-        <NavLink to="/admin/cuentos" style={linkStyle}  className={({ isActive }) =>
-    isActive ? "navlink navlink-active" : "navlink"
-  }>Cuentos</NavLink>
-        <NavLink to="/admin/usuarios" style={linkStyle}  className={({ isActive }) =>
-    isActive ? "navlink navlink-active" : "navlink"
-  }>Usuarios</NavLink>
-        <NavLink to="/admin/aulas" style={linkStyle}  className={({ isActive }) =>
-    isActive ? "navlink navlink-active" : "navlink"
-  }>Aulas</NavLink>
-        <NavLink to="/admin/actividades" style={linkStyle}  className={({ isActive }) =>
-    isActive ? "navlink navlink-active" : "navlink"
-  }>Actividades</NavLink>
-        <NavLink to="/admin/perfil" style={linkStyle}  className={({ isActive }) =>
-    isActive ? "navlink navlink-active" : "navlink"
-  }>Perfil</NavLink>
+      <hr style={{ width: "100%", padding: 0 }} />
+      <nav style={{ display: "grid", gap: 6, marginTop: "20px" }}>
+        <NavLink to="/admin" end className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }>
+          {" "}
+          <RiLayoutMasonryLine color="#0056D2" size={16} /> Dashboard
+        </NavLink>
+        <NavLink
+          to="/admin/cuentos"
+          className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }
+        >
+          {" "}
+          <FaBook color="#0056D2" size={16} />
+          Cuentos
+        </NavLink>
+        <NavLink
+          to="/admin/usuarios"
+          className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }
+        ><FaUser color="#0056D2" size={16}/>
+
+          Usuarios
+        </NavLink>
+        <NavLink
+          to="/admin/aulas"
+          className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }
+        > <FaBuilding color="#0056D2" size={16}/>
+
+          Aulas
+        </NavLink>
+        <NavLink
+          to="/admin/actividades"
+          className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }
+        ><FaClipboardList color="#0056D2" size={16}/>
+
+          Actividades
+        </NavLink>
+        <NavLink
+          to="/admin/perfil"          className={({ isActive }) =>
+            isActive ? "navlink navlink-active" : "navlink"
+          }
+        ><FaRegUser color="#0056D2" size={16}/>
+
+          Perfil
+        </NavLink>
       </nav>
-      <div style={{ marginTop: "auto", padding: 12 }}>
-        <button onClick={handleLogout} style={{ width: "100%", padding: "10px 12px", borderRadius: 8, border: "1px solid #e5e7eb", background: "#fff", cursor: "pointer" }}>Cerrar sesión</button>
+
       </div>
+
+      <div style={{ marginTop: "auto", padding: 12 }}>
+       <a href="" className="logout-link"> <FiLogOut size={16} /> Cerrar Sesion</a>
+        </div>
     </aside>
   );
 }
-
-
